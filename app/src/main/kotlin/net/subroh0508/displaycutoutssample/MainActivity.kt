21aa762c8f48e5f1bbcb6c055ff6ca991a6eecfa
subroh0508 <subroh.0508@gmail.com>
2021-03-17 00:35:11 +0900

    [edit]Enable status bar toggle action

--- app/src/main/kotlin/net/subroh0508/displaycutoutssample/MainActivity.kt
+++ app/src/main/kotlin/net/subroh0508/displaycutoutssample/MainActivity.kt
@@ -1,14 +1,20 @@
 package net.subroh0508.displaycutoutssample
 
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.IdRes
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import net.subroh0508.displaycutoutssample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private var statusBarHeight: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +24,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.topAppBar)
 
+        setOnSystemUiVisibilityChangeListener()
+
         displayFragment(R.id.fragment_container, MainFragment())
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if (window.decorView.systemUiVisibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+            statusBarHeight = Rect().let { window.decorView.getWindowVisibleDisplayFrame(it); it.top }
+        }
+    }
+
+    private fun setOnSystemUiVisibilityChangeListener() {
+        window.decorView.setOnSystemUiVisibilityChangeListener listener@ { visibility ->
+            if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                binding.messageBehindStatusBar.isVisible = false
+
+                return@listener
+            }
+
+            binding.messageBehindStatusBar.isVisible = true
+            binding.messageBehindStatusBar.updateLayoutParams<ViewGroup.LayoutParams> {
+                height = statusBarHeight
+            }
+        }
     }
 
     private fun displayFragment(
--- app/src/main/kotlin/net/subroh0508/displaycutoutssample/MainFragment.kt
+++ app/src/main/kotlin/net/subroh0508/displaycutoutssample/MainFragment.kt
@@ -7,10 +7,25 @@ import net.subroh0508.displaycutoutssample.databinding.FragmentMainBinding
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var binding: FragmentMainBinding
+    private val isVisibleStatusBar get() = activity?.window?.decorView?.systemUiVisibility == View.SYSTEM_UI_FLAG_VISIBLE
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMainBinding.bind(view)
+
+        binding.changeStatusBar.setOnClickListener {
+            if (isVisibleStatusBar) hideStatusBar() else showStatusBar()
+        }
+    }
+
+    private fun hideStatusBar() {
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //activity?.actionBar?.hide()
+    }
+
+    private fun showStatusBar() {
+        activity?.window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        //activity?.actionBar?.show()
     }
 }
